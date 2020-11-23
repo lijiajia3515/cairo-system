7dc1aba5955efbb48b2a8e74f4abaf8bca4a8439
lijiajia <lijiajia3515@163.com>
2020-11-23 11:42:21 +0800

    docker build

--- distribution/build.sh
+++ distribution/build.sh
@@ -1,2 +1,4 @@
 docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-system-service:snapshot -f system-service/Dockerfile ./system-service
+
+docker build -t registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-system-service:snapshot .
 docker push registry.cn-beijing.aliyuncs.com/hfhksoft/cairo-system-service:snapshot
