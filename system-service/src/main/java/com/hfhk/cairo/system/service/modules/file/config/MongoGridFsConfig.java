97fab174742e2a38da27ba21240f9d027b671cdd
lijiajia <lijiajia3515@163.com>
2020-12-09 17:45:03 +0800

    framework upgrade

--- system-service/src/main/java/com/hfhk/cairo/system/service/modules/file/config/MongoGridFsConfig.java
+++ system-service/src/main/java/com/hfhk/cairo/system/service/modules/file/config/MongoGridFsConfig.java
@@ -4,7 +4,7 @@ import com.hfhk.cairo.system.service.modules.file.constants.MongoConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
@@ -13,7 +13,7 @@ public class MongoGridFsConfig {
 
 	@Bean
 	@Primary
-	public GridFsTemplate harpoonGridFsTemplate(MongoDbFactory mongoDbFactory, MongoTemplate mongoTemplate) {
-		return new GridFsTemplate(mongoDbFactory, mongoTemplate.getConverter(), MongoConstants.BUCKET);
+	public GridFsTemplate harpoonGridFsTemplate(MongoDatabaseFactory mongoDatabaseFactory, MongoTemplate mongoTemplate) {
+		return new GridFsTemplate(mongoDatabaseFactory, mongoTemplate.getConverter(), MongoConstants.BUCKET);
 	}
 }
